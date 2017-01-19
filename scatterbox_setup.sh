3a32b4ed6b051f6d52d85ba9a86a2516ec97ef88
Ryan <ryan@burgettweb.net>
2017-01-19 14:53:30 -0500

    Update scatterbox_setup script

--- scatterbox_setup.sh
+++ scatterbox_setup.sh
@@ -6,6 +6,6 @@ cp /mnt/usb/LibraryBox/dir-generator.php /opt/piratebox/www/dir-generator.php
 
 # Update php.ini
 mv /etc/php.ini /etc/php-old.ini
-cp /mnt/usb/LibraryBox/php.ini /etc/php.ini
+cp /mnt/usb/scatterbox_setup_files/php.ini /etc/php.ini
 
 echo All done!
